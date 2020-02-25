53b2f479a44d921c6b62d0a6f771ba9cf5534153
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-02-25 21:24:59 +0900

    Use pkg/errors

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module nemotoy/ghaw
 
 go 1.13
+
+require github.com/pkg/errors v0.9.1
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+var Err = errors.New("")
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println(Err)
 }
